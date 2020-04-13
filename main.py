e26e7cf0f3556ffda58026e761c9030d12efd93f
Julian <julianheden@gmail.com>
2020-04-13 16:05:25 +0200

    UPDATE: minor improvements

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-import servo_simulator as s
-import co2_sensor_simulator as co2
+import servo as s
+import co2_sensor as co2
 import time
+import datetime
 
-
+# 21:44 close the window
 def save_data(oxygen, angle):
-	print("writing to file", oxygen, angle)
+	now = datetime.datetime.now().isoformat()
+	print("timestamp", now)
+	f = open("data.csv","a")
+	f.write(now + "," + str(oxygen) + "," + str(angle) + "\n")
+	f.close()
+
 
 def main():
 	servo = s.Servo()
-	# servo.SetAngle(10)
 	sensor = co2.Co2Sensor()
-	treshold = 400
-	# sensor.sense()
-
-
-	# 1. Read sensor
-	# 2. act on sensor data -> servo
-	# 3. save data to file
-	# 4. pause for a while
+	treshold = 900
 
 	while True:
 		oxygen = sensor.sense()
 		if oxygen > treshold:
-			servo.SetAngle(30)
+			servo.SetAngle(0)
 		else:
-			servo.SetAngle(10)
+			servo.SetAngle(90)
 
 		save_data(oxygen, servo.getRotation())
 
 		# time.sleep(300) # 5 min
-		time.sleep(5) # 5 seconds
+		time.sleep(10) # 5 seconds
 
 if __name__ == '__main__':
 	main()
+	# save_data(10, 90)
