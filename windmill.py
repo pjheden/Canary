42e5f12b5828370e59d033a89a6ffcd09294c31f
Julian <julianheden@gmail.com>
2020-08-29 23:12:27 +0200

    ADD button to spin

--- windmill.py
+++ windmill.py
@@ -12,10 +12,22 @@ class Windmill(object):
 		self.break_time = .2
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(self.pin, GPIO.OUT)
+		self.turn_off()
 
 	def __del__(self):
 		self.turn_off()
 		GPIO.cleanup()
+
+	def turn_on(self):
+		GPIO.output(self.pin, GPIO.LOW) # start engine
+
+	def turn_off(self):
+		GPIO.output(self.pin, GPIO.HIGH) # stop engine
+
+	# def turn_on_for_time(self, time):
+	# 	self.turn_on()
+	# 	time.sleep(time)
+	# 	self.turn_off()
 
 	def pulse(self, n):
 		for x in range(n):
@@ -25,9 +37,3 @@ class Windmill(object):
 			self.turn_off()
 
 			time.sleep(self.break_time) # break before new pulse
-
-	def turn_on(self):
-		g.output(self.pin, g.LOW) # start engine
-
-	def turn_off(self):
-		g.output(self.pin, g.HIGH) # stop engine
